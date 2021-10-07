23d485c5479542fae2e52d93f01fa6f5b204d77f
임예나(Lim Yena) <dpsk331@naver.com>
2021-10-07 09:31:16 +0900

    Feat(Transaction): Add Order Transaction

--- team6_project1/src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ team6_project1/src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -1,16 +1,8 @@
 package com.mycompany.webapp.controller;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
