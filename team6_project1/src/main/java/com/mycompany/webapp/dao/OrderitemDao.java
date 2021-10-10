1da67296f9028b0181418a3dc56c7331bb3f5a6e
woongity <ytw5754@naver.com>
2021-10-11 01:48:19 +0900

    feat($browser): get order history by term
    Added new events to $browser:
    - send get quest with period parameters to mypage controller to get order history in that period

--- team6_project1/src/main/java/com/mycompany/webapp/dao/OrderitemDao.java
+++ team6_project1/src/main/java/com/mycompany/webapp/dao/OrderitemDao.java
@@ -3,13 +3,14 @@ package com.mycompany.webapp.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-import com.mycompany.webapp.dto.OrderitemJoinProductJoinOrder;
-import com.mycompany.webapp.dto.OrderitemJoinProduct;
 import com.mycompany.webapp.dto.Orderitem;
+import com.mycompany.webapp.dto.OrderitemJoinProductJoinOrder;
 
 @Mapper
 public interface OrderitemDao {
 	public List<OrderitemJoinProductJoinOrder> selectOrderitemJoinProductJoinOrderinfoByMid(String mid);
 	public void insertOrderitem(Orderitem orderitem);
+	public List<OrderitemJoinProductJoinOrder> selectOrderitemJoinProductJoinOrderinfoByMidDate(@Param("mid") String mid,@Param("fromtime") String fromtime,@Param("totime") String totime);
 }
